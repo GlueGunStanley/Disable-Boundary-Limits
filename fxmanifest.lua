b49f8c60cbdd2daa818f0817e3650298b4458942
GlueGunStanley <kasonstanley@gmail.com>
2024-12-09 09:52:46 -0500

    Repo Established
    Established repository and resource file structure.

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Stanley Development Studios'
+description 'Disables the game worlds default boundary limits'
+
+github 'https://github.com/GlueGunStanley/Disable-Boundary-Limits'
+discord 'https://discord.com/invite/uCKZJed3Gq'
+
+client_script 'client/cl_main.lua'
